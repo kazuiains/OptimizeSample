681a6319a7daa1fc8d50aaea097ef1158e756d50
DESKTOP-H9VG51M\Adi <jakarta>
2025-05-27 09:18:13 +0700

    refactor: result

--- app/src/main/java/id/adiyusuf/optimizesample/screen/result/state/ResultStateNextScreen.kt
+++ app/src/main/java/id/adiyusuf/optimizesample/screen/result/state/ResultStateNextScreen.kt
@@ -1,7 +1,10 @@
 package id.adiyusuf.optimizesample.screen.result.state
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,18 +15,26 @@ import id.adiyusuf.optimizesample.AppComposition
 @Composable
 fun ResultStateNextScreen() {
     val navController = AppComposition.navigation
+    val scrollState = rememberScrollState()
 
-    Button(
+    Column(
         modifier = Modifier
             .padding(16.dp)
-            .fillMaxWidth(),
-        onClick = {
-            val savedStateHandle = navController.previousBackStackEntry?.savedStateHandle
-            savedStateHandle?.set("result_from", "SavedStateHandle - Composable")
-            savedStateHandle?.set("result_value", "Ululululu byur byar")
-            navController.popBackStack() // Kembali ke destinasi sebelumnya
-        }
+            .fillMaxWidth()
+            .verticalScroll(scrollState)
     ) {
-        Text("Send Result")
+        Button(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            onClick = {
+                val savedStateHandle = navController.previousBackStackEntry?.savedStateHandle
+                savedStateHandle?.set("result_from", "SavedStateHandle to Composable")
+                savedStateHandle?.set("result_value", "Ululululu byur byar")
+                navController.popBackStack() // Kembali ke destinasi sebelumnya
+            }
+        ) {
+            Text("Send Result")
+        }
     }
 }
